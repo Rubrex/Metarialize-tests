1ad6ff84a20c0e4b4199598521f545d80489d7d0
Rubrex <rubrex96@hotmail.com>
2020-05-20 12:58:36 +0600

    Updates

---
+++ precache-manifest.a379474f6f11f820b0a8a6e997d58de7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161375e4381973060356caea2d457f3a",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ef9f28cd20bcf18e8a9f",
+    "url": "/react-gh-pages/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "a71a604d416c0477d3d8",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef9f28cd20bcf18e8a9f",
+    "url": "/react-gh-pages/static/js/2.ea07f6ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.ea07f6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a71a604d416c0477d3d8",
+    "url": "/react-gh-pages/static/js/main.b639b5d2.chunk.js"
+  },
+  {
+    "revision": "804b2d3c5890e27174d8",
+    "url": "/react-gh-pages/static/js/runtime-main.2175d405.js"
+  }
+]);
