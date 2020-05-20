f39ebd436fb241cc89bccd72a2a41a6e7fbe47e1
Rubrex <rubrex96@hotmail.com>
2020-05-20 13:05:06 +0600

    app.js cleaned

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ function App() {
   };
   return (
     <div className="App">
-      <a class="waves-effect waves-light btn" href="#">
-        button
-      </a>
-      <a class="waves-effect waves-light btn " href="#">
-        <i class="material-icons left">cloud</i>button
-      </a>
-      <a class="waves-effect waves-light btn" href="#">
-        <i class="material-icons right">cloud</i>button
-      </a>
-      <a href="#" class="waves-effect waves-teal btn-flat" style={style}>
-        RubReX
-      </a>
       <Metal />
     </div>
   );
