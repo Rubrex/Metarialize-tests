1bbeddd6f507ef4dd067052def1442eb2467e17f
Rubrex <rubrex96@hotmail.com>
2020-05-23 01:30:37 +0600

    color

--- src/components/metarialize.jsx
+++ src/components/metarialize.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import "./style.css";
 
 const metarialize = () => {
   return (
     <div>
-      <a class="waves-effect waves-light btn">button</a>
-      <a class="waves-effect waves-light btn">
-        <i class="material-icons left">cloud</i>button
-      </a>
-      <a class="waves-effect waves-light btn">
-        <i class="material-icons right">cloud</i>button
-      </a>
+      <button class="waves-effect waves-light btn"></button>
+      <button class="waves-effect waves-light btn"></button>
+      <button class="waves-effect waves-light btn"></button>
+      <button class="waves-effect waves-light btn"></button>
+      <button class="waves-effect waves-light btn"></button>
     </div>
   );
 };
