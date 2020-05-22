813509b8cb2f595289b16939fdd9efdfd11ebc80
Rubrex <rubrex96@hotmail.com>
2020-05-23 02:47:18 +0600

    Dynamic Class

--- src/components/metarialize.jsx
+++ src/components/metarialize.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 
-const metarialize = () => {
-  return (
-    <div>
-      <button class="waves-effect waves-light btn"></button>
-      <button class="waves-effect waves-light btn"></button>
-      <button class="waves-effect waves-light btn"></button>
-      <button class="waves-effect waves-light btn"></button>
-      <button class="waves-effect waves-light btn"></button>
-    </div>
-  );
-};
+class metarialize extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { textColor: true };
+  }
+
+  render() {
+    const classes = [];
+    if (this.state.textColor) {
+      classes.push("greenc");
+    }
+    if (!this.state.textColor) {
+      classes.push("redc");
+    }
+    const changeColor = () => {
+      if (this.state.textColor) this.setState({ textColor: false });
+      else this.setState({ textColor: true });
+    };
+    return (
+      <div>
+        <div>
+          <h1 className={classes.join(" ")}>Change My Color</h1>
+          <button
+            class="waves-effect waves-light btn"
+            style={{ backgroundColor: "#28615b" }}
+            onClick={changeColor}
+          >
+            Switch Color
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default metarialize;
